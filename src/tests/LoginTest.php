91b33075373a9b6aefb1bacc87602eba6cfea795
akarisuki <akarisuki1222@gmail.com>
2023-12-12 13:49:14 +0900

    CSS調３

--- src/tests/LoginTest.php
+++ src/tests/LoginTest.php
@@ -1,7 +1,7 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
-require_once __DIR__ . '/../taishobutu_app/common/config.php';
+require_once (__DIR__ . '/../taishobutu_app/common/config.php');
 
 
 class LoginTest extends TestCase
